d95f786ae77ccc80f623448016a26d4ba3faf921
AlexDHoffer <hoffera@oregonstate.edu>
2017-10-14 17:44:36 -0700

    Finished

---
+++ HW3/README.md
@@ -0,0 +1,8 @@
+To compile:
+g++ change.cpp -o change
+
+To run:
+./change
+
+Must have amount.txt in directory
+Places results in change.txt
--- HW3/change.cpp
+++ HW3/change.cpp
@@ -13,37 +13,8 @@ using namespace std;
 
 ofstream outputFile; // Our output file.
 
-/*
-// Sort the vectors in the vector and print out the results to the output file.
-void processDoubleVec(vector < vector <int> > doubleVec)
-{
-	outputFile.open("stooge.out");
-
-	for (int i = 0; i < doubleVec.size(); i++)
-	{
-		vector <int> doubleVec2 = doubleVec.at(i);
-
-		//stoogeSort(doubleVec2, 0, doubleVec2.size() - 1);
-
-		for (int j = 0; j < doubleVec2.size(); j++)
-		{
-			outputFile << doubleVec2.at(j);
-
-			if (j != doubleVec2.size() -1)
-			{
-				outputFile << " ";
-			}
-		}
-
-		outputFile << "\n";
-	}
-
-
-	outputFile.close();
-
-}
-*/
-
+// We have a list of the numbers we've combined to get our result in changeResult
+// Now, map these to the coins to produce the output format requested.
 void adjustChangeResult(vector <int>& changeResult, vector <int> coins)
 {
 	vector <int> adjusted;
@@ -52,6 +23,8 @@ void adjustChangeResult(vector <int>& changeResult, vector <int> coins)
 	for (int i = 0; i < coins.size(); i++)
 		adjusted.push_back(0);
 
+	// Loop through all possible coins.
+	// For each coin, loop through the change result and if the coin is a match, increment the corresponding index's value.
 	for (int i = 0; i < coins.size(); i++)
 	{
 		int curValue = coins.at(i);
@@ -68,20 +41,24 @@ void adjustChangeResult(vector <int>& changeResult, vector <int> coins)
 	changeResult = adjusted;
 }
 
-// Find the vector of coins that were used
+// Find the vector of coins that were combined to reach our solution
 void findCoinsUsed(vector <int> coinsUsed, vector <int> coins, vector <int>& changeResult)
 {
+	// Start from the end...
 	int start = coinsUsed.size() - 1;
 
 	while (start != 0)
 	{
+		// Map the coinsUsed vector to their corresponding coin.
 		int j = coinsUsed.at(start);
 		changeResult.push_back(coins.at(j));
 		start = start - coins.at(j);
 	}
 
+	// We started at the end, so reverse the vector.
 	reverse(changeResult.begin(), changeResult.end());
 
+	// Get it in the right form.
 	adjustChangeResult(changeResult, coins);
 }
 
@@ -92,14 +69,16 @@ int changeMaker(vector <int> coins, int amount, vector <int>& changeResult)
 	vector <int> change(amount + 1);
 	vector <int> coinsUsed(amount + 1);
 
-	change.at(0) = 0;
+	change.at(0) = 0; // Having an amount of 0 means we used 0 coins.
 
 	for (int i = 1; i <= amount; i++)
 	{
-		change.at(i) = hugeValue;
-		coinsUsed.at(i) = -1;
+		change.at(i) = hugeValue; // The equivalent of inf.
+		coinsUsed.at(i) = -1; // To indicate we didn't use this coin yet.
 	}
 
+	// Think of this as a table of coins by increasing amounts until the amount we want
+	// If a coin is not greater than in value than the current max amount, make a decision to include it or not
 	for (int j = 0; j < coins.size(); j++)
 	{
 		for (int i = 1; i <= amount; i++)
@@ -109,12 +88,13 @@ int changeMaker(vector <int> coins, int amount, vector <int>& changeResult)
 				if (change.at(i - coins.at(j)) + 1 < change.at(i))
 				{
 					change.at(i) = 1 + change.at(i - coins.at(j));
-					coinsUsed.at(i) = j;
+					coinsUsed.at(i) = j; // We used the coin at index j, map it over later
 				}
 			}
 		}
 	}
 
+	// Map coin indices to actual coins.
 	findCoinsUsed(coinsUsed, coins, changeResult);
 
 	return change.at(amount);
@@ -125,35 +105,75 @@ void processVectors(vector < vector <int> > denominations, vector <int> amounts)
 	vector < vector <int> > changeResults;
 	vector <int> minNumCoinsNeeded;
 
+	// Get results for each denomination set and amount.
+	for (int i = 0; i < denominations.size(); i++)
+	{
+		vector <int> changeResult;
+		minNumCoinsNeeded.push_back(changeMaker(denominations.at(i), amounts.at(i), changeResult));
+		changeResults.push_back(changeResult);
+	}
+
 	outputFile.open("change.txt");
 	
-	vector <int> changeResult;
-	minNumCoinsNeeded.push_back(changeMaker(denominations.at(1), amounts.at(1), changeResult));
-	changeResults.push_back(changeResult);
+	// The output file will match the denominations and amounts but add two lines for each one.
+	int maxLines = 2 * (denominations.size() + amounts.size());
 
-	vector <int> tmp;
+	int printCounter = 0;
+	int denomCounter = 0;
+	int amtCounter = 0;
+	int resultCounter = 0;
+	int minCoinCounter = 0;
 
-	// Give denomination values to output file.
-	tmp = denominations.at(1);
+	// Printing out to output file in the order [ denom, amount, result vector, min number of coins ]
+	for (int i = 0; i < maxLines; i++)
+	{
+		vector <int> temp;
 
-	for (int i = 0; i < tmp.size(); i++)
-		outputFile << tmp.at(i) << " ";
+		if (printCounter == 0)
+		{
+			temp = denominations.at(denomCounter);
 
-	outputFile << endl;
+			for (int i = 0; i < temp.size(); i++)
+			{
+				outputFile << temp.at(i) << " ";
+			}
 
-	// Give the amount to output file.
-	outputFile << amounts.at(1) << endl;
+			outputFile << endl;
 
-	// Give the change result array to the output file.
-	tmp = changeResults.at(0); 
+			denomCounter += 1;
+		}	
 
-	for (int i = 0; i < tmp.size(); i++)
-		outputFile << tmp.at(i) << " ";
+		else if (printCounter == 1)
+		{
+			outputFile << amounts.at(amtCounter) << endl;
+			amtCounter += 1;
+		}
 
-	outputFile << endl;
+		else if (printCounter == 2)
+		{
+			temp = changeResults.at(resultCounter);
+			
+			for (int i = 0; i < temp.size(); i++)
+			{
+				outputFile << temp.at(i) << " ";
+			}
 
-	// Give the min number of coins used to the output file.
-	outputFile << minNumCoinsNeeded.at(0) << endl;
+			outputFile << endl;
+
+			resultCounter += 1;
+		}
+
+		else if (printCounter == 3)
+		{		
+			outputFile << minNumCoinsNeeded.at(minCoinCounter) << endl;
+			minCoinCounter += 1;
+		}
+
+		printCounter += 1;
+
+		if (printCounter == 4)
+			printCounter = 0;
+	}
 
 	outputFile.close();
 }
